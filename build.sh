629c970a5fbd42e851b20e320d748f6f46fa8656
Jimmy Manning <jimmymanning@gmail.com>
2019-03-02 10:54:56 -0600

    code cleanup

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -Wall -std=gnu99 temp.c -o w1 -lwiringPi -l wiringPiDev -lsqlite3
+gcc -Wall -std=gnu99 temp.c -o w1 -lwiringPi -l wiringPiDev -lsqlite3 -I../WiringPi/wiringPi/ -I../WiringPi/devLib
